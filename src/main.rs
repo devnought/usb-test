8d4e7e2746009f692f2f6fddd57434cd1be3cc6e
Kyle Gretchev <kyle.gretchev@magnetforensics.com>
2024-06-24 22:08:04 -0400

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+use nu_table::{NuTable, NuTableConfig, TableTheme};
+
 use vendors::Vendors;
 mod vendors;
 
 fn main() -> anyhow::Result<()> {
     let vendors = vendors::vendor_set();
     let iter = nusb::list_devices()?.filter(|d| vendors.contains(&d.vendor_id()));
+
+    let mut rows = vec![vec![
+        String::from("Vendor"),
+        String::from("Product"),
+        String::from("Serial"),
+    ]];
 
     for device_info in iter {
         let vendor_id = device_info.vendor_id();
@@ -21,13 +29,43 @@ fn main() -> anyhow::Result<()> {
         let is_apple = vendor_id == Vendors::Apple as u16;
 
         if is_android || is_apple {
-            println!(
-                "{vendor:?} - {:?} - {:?}",
-                device_info.product_string(),
-                device_info.serial_number()
-            );
+            let vendor = format!("{vendor:?}");
+
+            let product = if let Some(p) = device_info.product_string() {
+                p.to_string()
+            } else {
+                "N/A".to_string()
+            };
+
+            let serial = if let Some(s) = device_info.serial_number() {
+                s.to_string()
+            } else {
+                "N/A".to_string()
+            };
+
+            println!("{device_info:#?}");
+
+            let row = vec![vendor, product, serial];
+
+            rows.push(row);
         }
     }
+
+    let mut table = NuTable::new(rows.len(), 3);
+    for (row_index, row) in rows.into_iter().enumerate() {
+        for (col_index, col) in row.into_iter().enumerate() {
+            table.insert((row_index, col_index), col);
+        }
+    }
+
+    let table_cfg = NuTableConfig {
+        theme: TableTheme::rounded(),
+        with_header: true,
+        ..Default::default()
+    };
+
+    let output_table = table.draw(table_cfg, 80).unwrap();
+    println!("{output_table}");
 
     Ok(())
 }
